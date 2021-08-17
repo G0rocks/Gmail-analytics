c55a561db8bec79a7383419cf3df346436f3c651
Huldar <huldarh@gmail.com>
2021-08-17 11:10:43 +0000

    Probably made this better

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
 console.log("Popup says hello");
+
+browser.storage.local.get().then(function (result)) {
+  let results = [];
+
+  for (const key of Object.keys(result)) {
+    results.push(result);
+  }
+}
+
+let site_info = document.getElementsById("unread_ID")
+
+site_info.getElementsByClassName("unread_class")[0].textContent = results["Unreads"];
