9d1267a6d6f89efe4a6cc078ef0d15c0e4722554
Huldar <huldarh@gmail.com>
2021-08-18 16:52:45 +0000

    Now displays more values. Currently experiencing issue with readToday

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-console.log("Popup.js start");
+//console.log("Popup.js start");
 
 async function get_unreads() {
 
@@ -8,10 +8,8 @@ async function get_unreads() {
     function(value) { console.log("unreads promise success: " + value["Unreads"])
     return value["Unreads"]; },
     function(error) { console.log("unreads promise failed: " + error)
-    return NaN; }
+    return null; }
   );
-
-  console.log("Unreads: " + unreads);
 
   return unreads;
 }
@@ -20,8 +18,59 @@ async function set_popup_unreads(unreads){
   document.getElementById("unread_ID").innerHTML = "Unread: " + await unreads;
 }
 
-unreads = get_unreads();
+//unreads = get_unreads();
 
-set_popup_unreads(unreads);
+//set_popup_unreads(unreads);
+
+//----------------------------------------------------------------------
+//Test:
+
+function getData() {
+
+  let localStoragePromise = browser.storage.local.get();
+
+  unreads = localStoragePromise.then(
+    function(value) { //console.log("localStoragePromise success: " + value["Unreads"])
+    return value["Unreads"]; },
+    function(error) { console.log("localStoragePromise failed: " + error)
+    return null; }
+  );
+
+  readToday = localStoragePromise.then(
+    function(value) { //console.log("localStoragePromise success: " + value["Read Today"])
+    return value["Read_Today"]; },
+    function(error) { console.log("localStoragePromise failed: " + error)
+    return null; }
+  );
+
+  lasteUpdate = localStoragePromise.then(
+    function(value) { //console.log("localStoragePromise success: " + value["Last update"])
+    return value["Last update"]; },
+    function(error) { console.log("localStoragePromise failed: " + error)
+    return null; }
+  );
+
+  let storage = {
+    "Unreads": unreads,
+    "readToday": readToday,
+    "Last update": lasteUpdate
+  };
+
+  console.log("Storage: " + storage);
+
+  return storage;
+}
+
+async function writePopupData(localData){
+  document.getElementById("unread_ID").innerHTML = "Unread: " + await localData["Unreads"];
+  document.getElementById("read_today_ID").innerHTML = "Read_today: " + await localData["readToday"];
+  document.getElementById("last_update_ID").innerHTML = "Updated: " + await localData["Last update"];
+}
+
+let localData = getData();
+
+console.log("LocalData: " + localData["readToday"]);
+
+writePopupData(localData);
 
 console.log("Popup.js finished executing");
